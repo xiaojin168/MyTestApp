6cbf232e720d25d16eaf11ab086bf88943c5b4bc
xiaojin <jinzhenlu@macbook-pro.local>
2016-12-05 12:28:45 +0800

    Initial Commit

---
+++ MyTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTestApp
+//
+//  Created by xiaojin on 16/12/5.
+//  Copyright © 2016年 xiaojin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyTestApp
+//
+//  Created by xiaojin on 16/12/5.
+//  Copyright © 2016年 xiaojin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
